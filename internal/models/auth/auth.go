ee67db18958a4ec26270945a3a3db0abe8bbf2f6
Artem Klimenkov <amklimenkov752@yandex.ru>
2026-03-05 14:28:01 +0300

    Поправил модели

---
+++ internal/models/auth/auth.go
@@ -0,0 +1,15 @@
+package auth
+
+type RequestRegistrate struct {
+	Login    string `json:"login"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RequestLogin struct {
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type RequestLogout struct {
+}
