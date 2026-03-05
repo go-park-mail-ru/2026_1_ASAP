36b798ea8d650fd7ba1ac09b12780bfa3afa7880
Artem Klimenkov <amklimenkov752@yandex.ru>
2026-03-05 14:29:58 +0300

    поправил handlers

--- internal/handlers/auth/auth.go
+++ internal/handlers/auth/auth.go
@@ -3,29 +3,29 @@ package handlers
 import (
 	"net/http"
 
-	userService "github.com/go-park-mail-ru/2026_1_ASAP/internal/services/auth"
+	authService "github.com/go-park-mail-ru/2026_1_ASAP/internal/services/auth"
 )
 
 type AuthHandler struct {
-	AuthService userService.UserServiceInterface
+	AuthService authService.UserServiceInterface
 }
 
-func NewAuthHandler() *AuthHandler {
-	return &AuthHandler{}
+func NewAuthHandler(authService authService.UserServiceInterface) *AuthHandler {
+	return &AuthHandler{AuthService: authService}
 }
 
-func (api *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+func (authHeandler *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from login page"))
 }
 
-func (api *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+func (authHeandler *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from register page"))
 }
 
-func (api *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
+func (authHeandler *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from logout page"))
 }
 
-func (api *AuthHandler) Root(w http.ResponseWriter, r *http.Request) {
+func (authHeandler *AuthHandler) Root(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from root page"))
 }
