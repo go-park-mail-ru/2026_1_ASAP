861c446e8c19a81f39afc09ec1a33241ca201dcb
Artem Klimenkov <amklimenkov752@yandex.ru>
2026-03-07 13:59:07 +0300

    Валидация

--- internal/utils/validation/validation.go
+++ internal/utils/validation/validation.go
@@ -1,13 +1,144 @@
 package validation
 
-func ValidateEmail(email string) error {
-	return nil
+import (
+	"regexp"
+	"unicode"
+)
+
+type ValidationError struct {
+	Field   string
+	Message string
+	Code    string
 }
 
-func ValidateLogin(login string) error {
-	return nil
+var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]+$`)
+
+func ValidateEmail(email string) []ValidationError {
+	var errs []ValidationError
+
+	if email == "" {
+		errs = append(errs, ValidationError{
+			Field:   "email",
+			Message: "Email is required",
+			Code:    "EMAIL_REQUIRED",
+		})
+		return errs
+	}
+
+	if !emailRegex.MatchString(email) {
+		errs = append(errs, ValidationError{
+			Field:   "email",
+			Message: "Invalid email format",
+			Code:    "INVALID_EMAIL",
+		})
+	}
+
+	return errs
 }
 
-func ValidatePassword(password string) error {
-	return nil
+func ValidateLogin(login string) []ValidationError {
+	var errs []ValidationError
+
+	if login == "" {
+		errs = append(errs, ValidationError{
+			Field:   "login",
+			Message: "Login is required",
+			Code:    "LOGIN_REQUIRED",
+		})
+		return errs
+	}
+
+	if len(login) < 3 {
+		errs = append(errs, ValidationError{
+			Field:   "login",
+			Message: "Login must be at least 3 characters",
+			Code:    "LOGIN_TOO_SHORT",
+		})
+	}
+
+	return errs
+}
+
+func ValidatePassword(password string) []ValidationError {
+	var errs []ValidationError
+
+	if password == "" {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password is required",
+			Code:    "PASSWORD_REQUIRED",
+		})
+		return errs
+	}
+
+	if len(password) < 6 {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password must be at least 6 characters",
+			Code:    "PASSWORD_TOO_SHORT",
+		})
+	}
+
+	if len(password) > 64 {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password must be less than 64 characters",
+			Code:    "PASSWORD_TOO_LONG",
+		})
+	}
+
+	var hasUpper, hasLower, hasDigit, hasSpecial bool
+
+	for _, c := range password {
+		switch {
+		case unicode.IsUpper(c):
+			hasUpper = true
+		case unicode.IsLower(c):
+			hasLower = true
+		case unicode.IsDigit(c):
+			hasDigit = true
+		case unicode.IsSpace(c):
+			errs = append(errs, ValidationError{
+				Field:   "password",
+				Message: "Password must not contain spaces",
+				Code:    "PASSWORD_HAS_SPACE",
+			})
+		default:
+			hasSpecial = true
+		}
+	}
+
+	if !hasUpper {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password must contain at least one uppercase letter",
+			Code:    "PASSWORD_NO_UPPERCASE",
+		})
+	}
+
+	if !hasLower {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password must contain at least one lowercase letter",
+			Code:    "PASSWORD_NO_LOWERCASE",
+		})
+	}
+
+	if !hasDigit {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password must contain at least one digit",
+			Code:    "PASSWORD_NO_DIGIT",
+		})
+	}
+
+	if !hasSpecial {
+		errs = append(errs, ValidationError{
+			Field:   "password",
+			Message: "Password must contain at least one special character",
+			Code:    "PASSWORD_NO_SPECIAL",
+		})
+	}
+
+	return errs
 }
