846a21a7f1266dc254d6ac1f678d3a6b1cd21df7
sergey <mac@MacBook-Air-sergey.local>
2026-03-18 13:53:08 +0300

    переделали конфиг

--- config/config.go
+++ config/config.go
@@ -9,8 +9,10 @@ import (
 )
 
 type Config struct {
-	ServerConfig  ServerConfig
-	SessionConfig SessionConfig
+	ServerConfig   ServerConfig
+	SessionConfig  SessionConfig
+	RedisConfig    RedisConfig
+	PostgresConfig PostgresConfig
 }
 
 type ServerConfig struct {
@@ -21,6 +23,28 @@ type ServerConfig struct {
 type SessionConfig struct {
 	SessionTTL time.Duration
 }
+type RedisConfig struct {
+	Host     string
+	Port     string
+	Password string
+	Database int
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Database string
+}
+
+func (postgresConfig PostgresConfig) ServerInfo() string {
+	return postgresConfig.Host + ":" + postgresConfig.Port
+}
+
+func (redisConfig RedisConfig) ServerInfo() string {
+	return redisConfig.Host + ":" + redisConfig.Port
+}
 
 func (serverConfig ServerConfig) ServerInfo() string {
 	return serverConfig.Host + ":" + serverConfig.Port
@@ -29,6 +53,8 @@ func (serverConfig ServerConfig) ServerInfo() string {
 func LoadConfigFromEnv() (*Config, error) {
 	var serverConfig ServerConfig
 	var sessionConfig SessionConfig
+	var redisConfig RedisConfig
+	var postgresConfig PostgresConfig
 
 	host, err := getEnvVariable("HOST", "localhost")
 	if err != nil {
@@ -52,9 +78,71 @@ func LoadConfigFromEnv() (*Config, error) {
 		return nil, fmt.Errorf("Load Config error: %w", err)
 	}
 	sessionConfig.SessionTTL = time.Duration(sessionTTL) * time.Second
+
+	hostRedis, err := getEnvVariable("REDIS_HOST", "localhost")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	redisConfig.Host = hostRedis
+
+	portRedis, err := getEnvVariable("REDIS_PORT", "6379")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	redisConfig.Port = portRedis
+
+	passwordRedis, err := getEnvVariable("REDIS_PASSWORD", "")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	redisConfig.Password = passwordRedis
+
+	databaseRedisString, err := getEnvVariable("REDIS_DATABASE", "0")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+
+	databaseRedis, err := strconv.Atoi(databaseRedisString)
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	redisConfig.Database = databaseRedis
+
+	hostPostgres, err := getEnvVariable("POSTGRES_HOST", "localhost")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	postgresConfig.Host = hostPostgres
+
+	portPostgres, err := getEnvVariable("POSTGRES_PORT", "5432")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	postgresConfig.Port = portPostgres
+
+	usernamePostgres, err := getEnvVariable("POSTGRES_USER", "postgres")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	postgresConfig.Username = usernamePostgres
+
+	passwordPostgres, err := getEnvVariable("POSTGRES_PASSWORD", "postgres")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	postgresConfig.Password = passwordPostgres
+
+	databasePostgres, err := getEnvVariable("POSTGRES_DB", "postgres")
+	if err != nil {
+		return nil, fmt.Errorf("Load Config error: %w", err)
+	}
+	postgresConfig.Database = databasePostgres
+
 	config := &Config{
-		ServerConfig:  serverConfig,
-		SessionConfig: sessionConfig,
+		ServerConfig:   serverConfig,
+		SessionConfig:  sessionConfig,
+		RedisConfig:    redisConfig,
+		PostgresConfig: postgresConfig,
 	}
 
 	return config, nil
@@ -69,6 +157,6 @@ func getEnvVariable(variable string, defaultValue string) (string, error) {
 		log.Printf("Variable %s not found, using default %s", variable, defaultValue)
 		return defaultValue, nil
 	}
-
+	log.Printf("Variable %s found, using %s", variable, value)
 	return value, nil
 }
