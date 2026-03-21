278994e5598713080f9a4958782239486ff6cedb
sergey <mac@MacBook-Air-sergey.local>
2026-03-21 16:29:25 +0300

    поправил структуру

--- cmd/main.go
+++ cmd/main.go
@@ -52,26 +52,19 @@ func main() {
 	}))
 
 	sessRepo := sessionRepository.NewSessionRepository(cfg.SessionConfig, cfg.RedisConfig)
-	//depricatedSessRepo := sessionRepository.NewDepricatedSessionRepository()
 	sessionServ := session.NewSessionService(sessRepo, cfg.SessionConfig.SessionTTL)
-	//depricatedSessionServ := session.NewDepricatedSessionService(depricatedSessRepo, cfg.SessionConfig.SessionTTL)
 	userRepo, err := userRepository.NewUserRepository(context.Background(), cfg.PostgresConfig)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	//depricatedUserRepo := userRepository.NewDepricatedUserRepository()
 
 
 	chatRepo, err := chatRepository.NewChatRepository(context.Background(), cfg.PostgresConfig)
-	//depricatedchatRepo := chatRepository.NewDepricatedChatRepository()
 	authServ := authService.NewAuthService(userRepo, sessionServ)
 	chatServ := chatService.NewChatService(chatRepo, userRepo)
-	//depricatedchatServ := chatService.DepricatedNewChatService(depricatedchatRepo, depricatedUserRepo)
 	auth := authHandlers.NewAuthHandler(authServ)
-	//depricatedchatsHandler := chatHandlers.NewChatHandler(depricatedchatServ)
 	chatsHandler := chatHandlers.NewChatHandler(chatServ)
 	authMiddleware := middleware.AuthMiddleware(sessionServ)
-	//depricatedAuthMiddleware := middleware.DepricatedAuthMiddleware(depricatedSessionServ)
 	mux.Route("/api/v1/auth", func(mux chi.Router) {
 		mux.Post("/login", auth.Login)
 		mux.Post("/register", auth.Register)
