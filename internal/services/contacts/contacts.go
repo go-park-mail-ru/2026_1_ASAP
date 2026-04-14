c6ce86c480961199f7495ebdab6a5dfb54dea27e
sergey <mac@MacBook-Air-sergey.local>
2026-04-14 19:51:19 +0300

    моки для контактов тоже генерируются через go:generate

--- internal/services/contacts/contacts.go
+++ internal/services/contacts/contacts.go
@@ -11,6 +11,7 @@ import (
 	dto "github.com/go-park-mail-ru/2026_1_ASAP/internal/dto/contacts"
 )
 
+//go:generate mockgen -source=contacts.go -destination=mock/contacts_mock.go -package=mock
 type ContactRepositoryInterface interface {
 	GetAllContactsByUserID(ctx context.Context, userID int64) ([]*domain.Contact, error)
 	CreateContact(ctx context.Context, contact *domain.Contact) (*domain.Contact, error)
