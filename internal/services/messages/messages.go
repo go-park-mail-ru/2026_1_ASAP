5f826fa1e3c2609855303d63923cd2f44bac42b5
Artem Klimenkov <amklimenkov752@yandex.ru>
2026-04-15 00:33:35 +0300

    фикс

--- internal/services/messages/messages.go
+++ internal/services/messages/messages.go
@@ -13,6 +13,7 @@ import (
 
 const maxMessageRunes = 2000
 
+//go:generate mockgen -source=messages.go -destination=mock/messages_mock.go -package=mock
 type MessageRepositoryInterface interface {
 	CreateMessage(ctx context.Context, message *domain.Message) (*domain.Message, error)
 	GetMessagesByChatId(ctx context.Context, chatId int64, beforeID *int64, limit int) ([]*domain.Message, error)
